1dc13657724a6a15a68d92f5fcf6d14ec459fd85
liuzhi <1441765847@qq.com>
2018-07-24 00:05:43 +0800

    文章列表相关样式编写，布局编写

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,7 +3,7 @@
 <template>
   <div>
     <header-extend></header-extend>
-    <el-container style="margin: 0 300px">
+    <el-container style="margin: 0 200px">
       <el-main>
         <el-row :gutter="20" justify="space-between">
           <el-col :span="16">
@@ -25,17 +25,25 @@
           </el-col>
         </el-row>
         <el-container>
-          <el-main>
-            <el-card class="box-card">
-              <div v-html="richtext">
-                <!--{{richtext}}-->
-              </div>
-              <!--<div v-for="o in 10" :key="o" class="text item">-->
-                <!--{{'列表内容 ' + o }}-->
-              <!--</div>-->
+          <!--TODO 考虑只用row 不用 main-->
+          <el-main style="padding: 10px">
+            <el-card class="box-card" style="padding: 0;margin-bottom: 10px" v-for="o in loop_test" :key="o">
+              <el-row justify="space-between">
+                <el-col :span="6">
+                  <img style="height: 100%;width: 100%"
+                       src="https://vanliuzhi-object-storage.oss-cn-zhangjiakou.aliyuncs.com/2016411523810606.jpg?Expires=1532363044&OSSAccessKeyId=TMP.AQHsncAt-luwttwlRYjMZ1DaMeGgvQn7ScyDx-tWqaOytVYd-HhQAXBx_LuYAAAwLAIUH8rgiNQoUn78wmwLtCXryDTUozcCFC9KsmVGmJNNIEbObT5fX7yU51iw&Signature=eHF2Xppj8FfgsEOPBiouJOoYpTU%3D">
+                </el-col>
+                <el-col :span="18">
+                  <el-col :span="24">2018年7月23日</el-col>
+                  <el-col :span="24">python多线程编程</el-col>
+                  <el-col :span="24">
+                    <p style="-webkit-box-orient: vertical;" class="span_class abstract_c">今天也是充满希望的一天，有梦想的人不睡觉，到达星辰的彼岸,今天也是充满希望的一天，有梦想的人不睡觉，到达星辰的彼岸,今天也是充满希望的一天，有梦想的人不睡觉，到达星辰的彼岸,今天也是充满希望的一天，有梦想的人不睡觉，到达星辰的彼岸</p>
+                  </el-col>
+                </el-col>
+              </el-row>
             </el-card>
           </el-main>
-          <el-aside>
+          <el-aside style="padding: 10px">
             <el-card class="box-card">
               <div v-for="o in 4" :key="o" class="text item">
                 {{'列表内容 ' + o }}
@@ -46,7 +54,7 @@
 
       </el-main>
       <!--<el-aside width="200px">-->
-        <!--lll-->
+      <!--lll-->
       <!--</el-aside>-->
     </el-container>
     <el-footer class="footer_class">
@@ -99,7 +107,8 @@
         "<td style=\"width: 20%;\">&nbsp;</td>\n" +
         "</tr>\n" +
         "</tbody>\n" +
-        "</table>"
+        "</table>",
+        loop_test: [1, 2, 3, 4, 5]
       }
     }
   }
@@ -109,5 +118,17 @@
   .footer_class {
     /*position: fixed;*/
     /*border: 0;*/
+  }
+  .span_class:hover{
+    color: red;
+  }
+  .span_class {
+    color: #17a2b8;;
+  }
+  .abstract_c {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
   }
 </style>
