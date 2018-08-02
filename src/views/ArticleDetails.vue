28e60e9102d8a5d8df45766ee2216f7933cf70ed
liuzhi <1441765847@qq.com>
2018-08-02 17:27:06 +0800

    详情页面处理

--- src/views/ArticleDetails.vue
+++ src/views/ArticleDetails.vue
@@ -3,7 +3,7 @@
 <template>
   <div>
     <header-extend></header-extend>
-    <el-container style="margin: 0 200px">
+    <el-container style="margin-left: auto;margin-right: auto; width: 1100px">
       <el-main>
         <el-row justify="start" style="margin: 5px">
           <el-col :span="4">
@@ -11,28 +11,28 @@
           </el-col>
           <!--<el-col :span="3"><el-button type="primary">返回</el-button></el-col>-->
         </el-row>
-        <el-card style="text-align: left;padding: 50px" v-html="content">
+        <el-card style="text-align: left;padding: 50px" class="rich_content_class" v-html="content">
 
         </el-card>
       </el-main>
-      <el-aside>
-        <div class="rich_text_class" v-html="menu">
+      <el-aside style="position: relative;">
+        <div class="rich_text_class" style="position:fixed;" v-html="menu">
 
         </div>
         <ol class="rich_text_class">
-          <li>I have never seen anything like this</li>
-          <li>This is most certainly cool, and it can also work on long texts inside the list-item elements, which spans over multiple lines</li>
-          <li>Un-ordered lists are fun to play with.</li>
-          <li>Firefox sure does animate this effect nicely.</li>
-          <li>This is so simple
-            <!--<ol>-->
-              <!--<li>I have never seen anything like this</li>-->
-              <!--<li>This is most certainly cool, and it can also work on long texts inside the list-item elements, which spans over multiple lines</li>-->
-              <!--<li>Un-ordered lists are fun to play with.</li>-->
-              <!--<li>Firefox sure does animate this effect nicely.</li>-->
-              <!--<li>This is so simple</li>-->
-            <!--</ol>-->
-          </li>
+          <!--<li>I have never seen anything like this</li>-->
+          <!--<li>This is most certainly cool, and it can also work on long texts inside the list-item elements, which spans over multiple lines</li>-->
+          <!--<li>Un-ordered lists are fun to play with.</li>-->
+          <!--<li>Firefox sure does animate this effect nicely.</li>-->
+          <!--<li>This is so simple-->
+          <!--<ol>-->
+          <!--<li>I have never seen anything like this</li>-->
+          <!--<li>This is most certainly cool, and it can also work on long texts inside the list-item elements, which spans over multiple lines</li>-->
+          <!--<li>Un-ordered lists are fun to play with.</li>-->
+          <!--<li>Firefox sure does animate this effect nicely.</li>-->
+          <!--<li>This is so simple</li>-->
+          <!--</ol>-->
+          <!--</li>-->
         </ol>
       </el-aside>
     </el-container>
@@ -51,8 +51,13 @@
     //     required: true
     //   }
     // },
+    methods: {
+      positioningToDom(v){
+        console.log(v)
+        v.target.scrollIntoView()
+      }
+    },
     created() {
-      console.log(this.$route.params.data)
       this.content = this.$route.params.data.data.content
       this.menu = this.$route.params.data.data.menu
     },
@@ -66,9 +71,17 @@
 </script>
 
 <style scoped>
+  .rich_content_class /deep/ h2, .rich_content_class /deep/ h3 {
+    margin: 10px 0 10px 0;
+    padding: 0 10px;
+    border-left: 5px solid #20c997;
+    background-color: #f7f7f7;
+    line-height: 2em;
+  }
+
   /*.rich_text /deep/ .rich_text_class {*/
-    /*overflow: hidden;*/
-    /*counter-reset: xxx 0;*/
+  /*overflow: hidden;*/
+  /*counter-reset: xxx 0;*/
   /*}*/
   .rich_text_class /deep/ li {
     list-style: none;
@@ -78,6 +91,7 @@
     -webkit-transition: .12s;
     transition: .12s;
   }
+
   .rich_text_class /deep/ li::before {
     position: absolute;
     content: '\2022';
@@ -91,9 +105,19 @@
     -webkit-transition: .5s;
     transition: .5s;
   }
+
   .rich_text_class /deep/ li:hover {
     color: red;
   }
+
+  .rich_text_class /deep/ a {
+    color: #3a8ee6;
+  }
+
+  .rich_text_class /deep/ a:hover {
+    color: red;
+  }
+
   .rich_text_class /deep/ li:hover::before {
     -webkit-transform: scale(2);
     -ms-transform: scale(2);
@@ -103,8 +127,9 @@
     -webkit-transition: .1s;
     transition: .1s;
   }
+
   .rich_text_class /deep/ li::before {
-    content: counter(xxx,decimal) ".";
+    content: counter(xxx, decimal) ".";
     counter-increment: xxx 1;
     font-size: 1em;
     opacity: .5;
@@ -112,6 +137,7 @@
     -webkit-transition: .5s;
     transition: .5s;
   }
+
   .rich_text_class /deep/ li:hover:before {
     opacity: 1;
     left: -10px;
@@ -135,4 +161,72 @@
     margin: 2%;
     /*box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);*/
   }
+
+  @media (min-width: 1200px) {
+    .container {
+      max-width: 1140px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .container {
+      max-width: 970px;
+      padding-right: 15px;
+      padding-left: 15px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .container {
+      max-width: 750px;
+      padding-right: 12px;
+      padding-left: 12px;
+    }
+  }
+
+  @media (min-width: 576px) {
+    .container {
+      max-width: none;
+      padding-right: 10px;
+      padding-left: 10px;
+    }
+  }
+
+  .container {
+    padding-right: 5px;
+    padding-left: 5px;
+  }
+
+  @media (min-width: 1200px) {
+    .container {
+      max-width: 1140px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .container {
+      max-width: 960px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .container {
+      max-width: 720px;
+    }
+  }
+
+  @media (min-width: 576px) {
+    .container {
+      max-width: 540px;
+    }
+  }
+
+  .container {
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+
 </style>
